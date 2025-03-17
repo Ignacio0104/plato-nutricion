117d660475474c742fa5d6aff2409fba5af7fe6f
Ignacio Martin Smirlian <ignacio.smirlian@hotmail.com>
2025-03-16 21:03:31 -0300

    suspense added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import MainLayout from "./components/MainLayout/MainLayout";
 
 export default function Home() {
   return (
     <main className="w-full h-full flex flex-col items-center justify-center pt-6">
-      <MainLayout />
+      <Suspense fallback={<div>Loading...</div>}>
+        <MainLayout />
+      </Suspense>
     </main>
   );
 }
